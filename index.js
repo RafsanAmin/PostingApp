d1b304f5d1fccff1533095a9c4fa2845402ae635
hrmrafsan <59786405+RafsanAmin@users.noreply.github.com>
2021-06-22 11:32:30 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,44 +1,60 @@
 const express = require("express");
+const next = require("next");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const ENV = require("dotenv").config({ path: __dirname + "/.env" });
-const csurf = require("csurf");
 const app = express();
 const UserHandle = require("./Posting Apps/Routes/UserHandle");
 const cookieParser = require("cookie-parser");
 //init and middlewares
+//
+//
+//
+const dev = process.env.NODE_ENV !== "production";
 
 const DB_KEY = process.env["DB_KEY"];
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
-app.use(cookieParser());
-// app.use(csurf({ cookie: true }));
-app.use("/uh", UserHandle);
-app.use("/index", express.static(`./static`));
-//connection to database
-
-mongoose
-  .connect(DB_KEY, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-  })
+const nextApp = next({ dev });
+const handle = nextApp.getRequestHandler();
+//
+//
+//
+nextApp
+  .prepare()
   .then(() => {
-    console.log("Connected");
+    app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+    app.use(cookieParser());
+    // app.use(csurf({ cookie: true }));
+    app.use("/uh", UserHandle);
+    app.use("/index", express.static(`./static`));
+    //connection to database
+
+    mongoose
+      .connect(DB_KEY, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+      })
+      .then(() => {
+        console.log("Connected");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    //routes
+
+    //routes
+    app.get("*", (req, res) => {
+      return handle(req, res);
+    });
+
+    //start server
+    //start server
+
+    app.listen(process.env.PORT, () => {
+      console.log("Listening to the port 80");
+    });
   })
-  .catch((err) => {
-    console.log(err);
+  .catch((ex) => {
+    console.error(ex.stack);
+    process.exit(1);
   });
-
-//routes
-
-app.get("/", async (req, res) => {
-  let x = req.csrfToken;
-  console.log(x);
-  res.send("Hello");
-});
-
-//start server
-
-app.listen(80, () => {
-  console.log("Listening to the port 80");
-});
